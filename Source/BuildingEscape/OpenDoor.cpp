32ac795fc9666ba160a228b30be5f0d0075bd91c
menevia16a <menevia16a@gmail.com>
2017-07-18 03:07:47 -0400

    Setup door swing.

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -30,36 +30,19 @@ void UOpenDoor::BeginPlay()
 	}
 }
 
-void UOpenDoor::OpenDoor()
-{
-	//Set the door rotation
-	Owner->SetActorRotation(FRotator(0.f, OpenAngle, 0.f));
-}
-
-void UOpenDoor::CloseDoor()
-{
-	//Set the door rotation
-	Owner->SetActorRotation(FRotator(0.f, 0.f, 0.f));
-}
-
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-
-
 	//Poll the Trigger Volume every frame
 	if (GetTotalMassOfActorsOnPlate() > TriggerMass)
 	{
-		OpenDoor();
-		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+		OnOpen.Broadcast();
 	}
-
-	//Check if it's time to close the door
-	if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay)
+	else
 	{
-		CloseDoor();
+		OnClose.Broadcast();
 	}
 }
 
--- Source/BuildingEscape/OpenDoor.h
+++ Source/BuildingEscape/OpenDoor.h
@@ -7,6 +7,8 @@
 #include "Components/ActorComponent.h"
 #include "OpenDoor.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpen);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnClose);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
@@ -21,28 +23,24 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-	void OpenDoor();
-	void CloseDoor();
-
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	UPROPERTY(BlueprintAssignable)
+		FOnOpen OnOpen;
+
+	UPROPERTY(BlueprintAssignable)
+		FOnClose OnClose;
+
 private:
 	//UPROPERTY is Unreal engine specific, read documentation.
-	UPROPERTY(EditAnywhere)
-		float OpenAngle = 90.f;
-
 	UPROPERTY(EditAnywhere)
 		ATriggerVolume* PressurePlate = nullptr;
 
 	UPROPERTY(EditAnywhere)
-		float DoorCloseDelay = 1.f;
-
-	UPROPERTY(EditAnywhere)
 		float TriggerMass = 0.f;
 
-	float LastDoorOpenTime;
 	float GetTotalMassOfActorsOnPlate(); //Returns total mass in kg
 
 	AActor* Owner = nullptr;
